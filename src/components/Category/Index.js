7a0f814fbdd43ad949ebe03ada333a17567e9aed
BhavyaBWappgo <bhavyawappgo@gmail.com>
2023-06-24 17:09:16 +0530

    change category page

--- src/components/Category/Index.js
+++ src/components/Category/Index.js
@@ -1,67 +1,90 @@
 import React, { useState } from 'react'
 import InputformComp from '../InputFormComp'
+import ToggleButton from '../ToggleButton'
 
-const EmptySubCategory= {
-    name:"",
-    imgeUrl
+const EmptySubCategory = {
+    name: "",
+    imgeUrl: ''
 }
 
 const emptyCategory = {
     name: "",
-    imageUrl:"",
-    subCategories: [
+    imageUrl: "",
+    subcategory: [
     ]
+
 }
 
 
-const Category = () =>{
-    const [categories, setCategories] = useState([])
+
+const Category = () => {
+    const [category, setCategories] = useState([])
 
     function AddCategory() {
-        setCategories([...categories, { ...emptyCategory }])
+        setCategories([...category, { ...emptyCategory }])
     }
-    console.log(categories)
+    console.log(category)
 
     function handleCategoryChange(value, index) {
-        const categoriesCopy = [...categories];
-        categoriesCopy[index].name = value;
-        setCategories(categoriesCopy)
+        const categoryCopy = [...category];
+        categoryCopy[index].name = value;
+        setCategories(categoryCopy)
 
     }
     function AddSubCategory(index) {
-        const categoriesCopy = JSON.parse(JSON.stringify(categories));
-        console.log(categoriesCopy)
-        categoriesCopy[index].subCategories.push(EmptySubCategory)
-        setCategories(categoriesCopy)
+        const categoryCopy = JSON.parse(JSON.stringify(category));
+        console.log(categoryCopy)
+        categoryCopy[index].subcategory.push(EmptySubCategory)
+        setCategories(categoryCopy)
     }
-    function handleSabCategoryName(categoryIndex, sabVaiantIndex, value) {
-        const categoriesCopy = JSON.parse(JSON.stringify(categories));
-        categoriesCopy[categoryIndex].subCategories[sabVaiantIndex].name = value
-        setCategories(categoriesCopy)
+    function handleSabCategoryName(variantIndex, sabCategoryIndex, value) {
+        const categoryCopy = JSON.parse(JSON.stringify(category));
+        categoryCopy[variantIndex].subcategory[sabCategoryIndex].name = value
+        setCategories(categoryCopy)
     }
-    function handleSabCategoryPrice(categoryIndex, sabVaiantIndex, value) {
-        const categoriesCopy = JSON.parse(JSON.stringify(categories));
-        categoriesCopy[categoryIndex].subCategories[sabVaiantIndex].price = value
-        setCategories(categoriesCopy)
+    function handleSabCategoryPrice(variantIndex, sabCategoryIndex, value) {
+        const categoryCopy = JSON.parse(JSON.stringify(category));
+        categoryCopy[variantIndex].subcategory[sabCategoryIndex].price = value
+        setCategories(categoryCopy)
     }
-
+    function removedescription(variantIndex) {
+        let categoryCopy = [...category];
+        categoryCopy.splice(variantIndex, 1);
+        setCategories(categoryCopy)
+    }
     return (
-        <div>
-            <button className="bg-green-700 mx-2 my-2 px-2 py-2 text-center text-white rounded-full w-full" onClick={() => AddCategory()}>Add Category</button>
-            {categories.map((category, categoryIndex) => {
-                return <div>
-                    <div className='flex'>
-                    <InputformComp label={categoryIndex + 1} text={category.name} setText={(value) => handleCategoryChange(value, categoryIndex)} />
-                    <button className="bg-green-700 mx-2 my-2 px-2 py-2 text-center text-white rounded-full w-full" onClick={()=> AddSubCategory(categoryIndex)}>+</button>
+        <div className='justify-center flex'>
+            <div className="w-auto xl:w-4/6 space-y-3 ">
+                <div className='flex items-center '>
+                    <label className="block text-gray-500 font-bold md:text-right mb-1 ml-5 md:mb-0 pr-4" for="inline-full-name">
+                        Category
+                    </label>
+                    <button className="bg-green-700 max-w-full mx-2 my-2 px-2 py-2 text-center text-white rounded-full w-full" onClick={() => AddCategory()}>Add Category</button>
+
                 </div>
-                {category.subCategories.map((subVar, subVarIndex)=>{
-                    return <div className='flex'>
-                        <InputformComp label={"Name"} text={subVar.name} setText={(value) => handleSabCategoryName(categoryIndex,subVarIndex, value)} />
-                        <InputformComp label={"Price"} text={subVar.price} setText={(value) => handleSabCategoryPrice(categoryIndex,subVarIndex, value)} />
+                {category.map((variant, variantIndex) => {
+                    return <div className='border p-3'>
+                        <div className="flex items-center float-right">
+                            <ToggleButton isSelected="dcndn" setIsSelected="dchd" rightLabel="Available" leftLabel="Not Available" />
                         </div>
+                        <div className='flex items-center gap-5 mb-9 pt-9'>
+                            <div className="md:w-3/3">
+                                <InputformComp label={variantIndex + 1} text={variant.name} setText={(value) => handleCategoryChange(value, variantIndex)} /></div>
+                            <button className="font-bold  mb-6 hover:text-red-400 "><img className="inline-block" src="public/assets/file-upload.png" /></button>
+                            <button className="font-bold  mb-6 hover:text-red-400 " onClick={() => AddSubCategory(variantIndex)}><img className="inline-block" src="https://img.icons8.com/color/30/000000/plus--v3.png" /></button>
+                            <button className="font-bold  mb-6 hover:text-red-400" onClick={() => removedescription(variantIndex)}><img src="https://img.icons8.com/plasticine/40/000000/filled-trash.png" /></button>
+                        </div>
+                        {variant.subcategory.map((subVar, subVarIndex) => {
+                            return <div className='flex items-center gap-5'>
+                                <InputformComp label={"Name"} text={subVar.name} setText={(value) => handleSabCategoryName(variantIndex, subVarIndex, value)} />
+
+                                <button className="font-bold  mb-6 hover:text-red-400" onClick={() => removedescription(variantIndex)}><img src="https://img.icons8.com/plasticine/40/000000/filled-trash.png" /></button>
+                                <div className='mb-5'><ToggleButton isSelected="dcndn" setIsSelected="dchd" /></div>
+                            </div>
+                        })}
+                    </div>
                 })}
-                </div>
-            })}
+            </div>
         </div>
     )
 }

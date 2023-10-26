9c1f37eff5c8299dfb79c98d504f482651b1e47c
Tejas <tejas.goel786@gmail.com>
2023-10-26 12:28:24 +0530

    fixed image issue added sort number in subCat

--- src/components/Category/SubCategoryBox.js
+++ src/components/Category/SubCategoryBox.js
@@ -9,13 +9,15 @@ const SubCategoryBox = (props) => {
   const [showModel, setShowModel] = useState(false);
   const [imgUrl, setImgUrl] = useState(subCat.imgUrl);
   const [subCatName, setSubCatName] = useState(subCat.name);
+  const [subCatNumber, setSubCatNumber] = useState(subCat.sortNumber || "");
 
   useEffect(() => {
     const categoriesCopy = JSON.parse(JSON.stringify(categories));
     categoriesCopy[categoryIndex].subCategories[subCatIndex].imgUrl = imgUrl;
     categoriesCopy[categoryIndex].subCategories[subCatIndex].name = subCatName;
+    categoriesCopy[categoryIndex].subCategories[subCatIndex].sortNumber = subCatNumber;
     setCategories(categoriesCopy);
-  }, [imgUrl, subCatName]);
+  }, [imgUrl, subCatName, subCatNumber]);
 
   function removeSubCategory() {
     const categoriesCopy = JSON.parse(JSON.stringify(categories));
@@ -24,32 +26,49 @@ const SubCategoryBox = (props) => {
   }
 
   function handleUrl(url) {
-    console.log("url",url)
+    console.log("url", url);
     setImgUrl(url);
   }
-  console.log(showModel)
+  console.log(showModel);
   return (
     <div className="bg-white my-2 p-4 rounded-md md:col-span-1">
-      <h1 className="font-bold text-lg py-2">Sub Category {subCatIndex + 1}</h1>
+      <div className="flex">
+        <h1 className="font-bold text-lg py-2">
+          Sub Category {subCatIndex + 1}
+        </h1>
+        <button
+          className="font-bold text-red-400 hover:text-red-600"
+          onClick={() => removeSubCategory()}
+        >
+          <img
+            src="https://img.icons8.com/plasticine/40/000000/filled-trash.png"
+            alt="Remove Sub Category"
+          />
+        </button>
+      </div>
 
       <div className="flex flex-col md:flex-row justify-between items-center gap-3">
         <div className="flex items-center gap-3 flex-1">
-          <label className="font-bold">Name</label>
-          <input
-            type="text"
-            value={subCatName}
-            onChange={(e) => setSubCatName(e.target.value)}
-            className="border rounded-md px-2 py-1 w-full"
-          />
-          <button
-            className="font-bold text-red-400 hover:text-red-600"
-            onClick={() => removeSubCategory()}
-          >
-            <img
-              src="https://img.icons8.com/plasticine/40/000000/filled-trash.png"
-              alt="Remove Sub Category"
+          <div>
+            <label className="font-bold">Name</label>
+            <input
+              type="text"
+              value={subCatName}
+              onChange={(e) => setSubCatName(e.target.value)}
+              className="border rounded-md px-2 py-1 w-full"
             />
-          </button>
+            <label className="font-bold mt-5">Sort Number</label>
+            <input
+              type="text"
+              value={subCatNumber}
+              onChange={(e) => {
+                let value = e.target.value
+                if(isNaN(value)) return
+                setSubCatNumber(value)
+              }}
+              className="border rounded-md px-2 py-1 w-full"
+            />
+          </div>
         </div>
         <div>
           {showModel && (
